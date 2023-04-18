3ed584a16716ac7060ecb0fe983e01430e8e39d7
mathiasck <mathias.ck2000@gmail.com>
2023-04-18 12:57:45 +0200

    Create redis module

--- server/src/todos/todos.module.ts
+++ server/src/todos/todos.module.ts
@@ -1,9 +1,10 @@
+import { redisModule } from '@/modules.config';
 import { Module } from '@nestjs/common';
 import { TodosController } from '@todos';
 import { TodosService } from '@todos/todos.service';
 
 @Module({
-  imports: [],
+  imports: [redisModule],
   controllers: [TodosController],
   providers: [TodosService],
 })
